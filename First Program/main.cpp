85a1bf6940d468ea79a43ec1b21ac6537f3bd795
Debajit Das <151544942+debajit-das@users.noreply.github.com>
2024-04-17 16:18:38 +0530

    Delete First Program/main.cpp

--- First Program/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-
-int main(){
-    cout << "Hello World" << endl;  // endl takes the cursor to the next line
-    cout << "Hello World" << endl;
-    return 0;
-}
