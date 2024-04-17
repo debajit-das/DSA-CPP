d7834e4d76948b9bddf144c0b5f95705d5f60f9c
Debajit Das <debas1118@gmail.com>
2024-04-17 23:15:23 +0530

    Taking input from user

---
+++ C++ Programing Basics/User Input/input.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int age;
+    cout << "Enater your age :" ;
+    cin >> age;   // taking inut from user
+    cout << "Your age is :" << age << endl;
+
+    return 0;
+}
+// homework
+// cin.ignore()
+// cin.fail()
+// getline(cin , 10)
