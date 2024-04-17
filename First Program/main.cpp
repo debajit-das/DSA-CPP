5c801f4a7eeef723fb83b03a644dacc388ca4132
Debajit Das <debas1118@gmail.com>
2024-04-17 15:58:26 +0530

    Hello world in C++

---
+++ First Program/main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    cout << "Hello World" << endl;  // endl takes the cursor to the next line
+    cout << "Hello World" << endl;
+    return 0;
+}
