db8f26807c13d5e044b16965d35941bff40e7d0d
Debajit Das <debas1118@gmail.com>
2024-04-20 09:33:52 +0530

    Nested loops

--- C++ Programing Basics/Loops/do-while.cpp
+++ C++ Programing Basics/Loops/do-while.cpp
@@ -2,6 +2,14 @@
 using namespace std;
 
 int main(){
+    int i = 1;
+    int sum = 0;
+    do
+    {
+        sum = sum + i;
+        i++;
+    } while (i <= 10);
+    cout << "Sum of 1 to 10 is: " << sum << endl;
 
     return 0;
 }
