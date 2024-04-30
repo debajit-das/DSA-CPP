98ba47f03e4fcb2205a5c19568927a68e59b34eb
Debajit Das <debas1118@gmail.com>
2024-04-30 22:40:42 +0530

    Swapping two numbers using funtions

---
+++ C++ Practice/swapfunction.cpp
@@ -0,0 +1,26 @@
+// Write a Program to Swap Two Numbers Using a Function.
+
+#include <iostream>
+using namespace std;
+
+void swap(int n1, int n2)
+{
+    n1 = n1 - n2;
+    n2 = n1 + n2;
+    n1 = n2 - n1;
+    cout << "After swapping the first number is : " << n1 << " and second number is : " << n2 << endl;
+}
+
+int main()
+{
+    int n1, n2;
+    cout << "Enter the first number : ";
+    cin >> n1;
+    cout << "Enter the second number : ";
+    cin >> n2;
+
+    cout << "Before swapping the first number is : " << n1 << " and second number is : " << n2 << endl;
+    swap(n1, n2);
+
+    return 0;
+}
