4bf839b109005fc83e7c4eb7d69daee462aafc3d
Debajit Das <debas1118@gmail.com>
2024-05-04 18:04:39 +0530

    Strings

---
+++ C++ Programing Basics/strings/string.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    char s[] = "Debajit Das";
+    cout << s << endl;
+
+    // declare
+    string name = "Debajit";
+    string surname = "Das";
+    cout << name + surname << endl;
+
+    string str;
+    cout << "Enter String" << endl;
+    cin >> str;
+    cout << "String is: " << str << endl;
+
+    return 0;
+}
