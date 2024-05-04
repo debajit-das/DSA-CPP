c4f99177c05a2c266e9146271687d6d24e5abd92
Debajit Das <debas1118@gmail.com>
2024-05-04 18:03:16 +0530

    Update comaring two arrays

--- C++ Practice/comparTwoArray.cpp
+++ C++ Practice/comparTwoArray.cpp
@@ -1,15 +1,13 @@
-// C++ program to find given two array
-// are equal or not
+// C++ program to find given two array are equal or not
 #include <iostream>
 using namespace std;
 
 // Returns true if arr1[0..n-1] and arr2[0..m-1]
 // contain same elements.
-bool areEqual(int arr1[], int arr2[], int N, int M)
+bool areEqual(int arr1[], int arr2[], int n, int m)
 {
-    // If lengths of array are not equal means
-    // array are not equal
-    if (N != M)
+    // If lengths of array are not equal means array are not equal
+    if (n != m)
         return false;
 
     // Sort both arrays
@@ -17,7 +15,7 @@ bool areEqual(int arr1[], int arr2[], int N, int M)
     // sort(arr2, arr2 + M);
 
     // Linearly compare elements
-    for (int i = 0; i < N; i++)
+    for (int i = 0; i < n; i++)
         if (arr1[i] != arr2[i])
             return false;
 
@@ -25,18 +23,20 @@ bool areEqual(int arr1[], int arr2[], int N, int M)
     return true;
 }
 
-// Driver's Code
 int main()
 {
-    int arr1[] = {3, 5, 2, 5, 2};
-    int arr2[] = {2, 3, 5, 5, 2};
-    int N = sizeof(arr1) / sizeof(int);
-    int M = sizeof(arr2) / sizeof(int);
+    int arr1[] = {3, 5, 2, 5, 2, 4};
+    int arr2[] = {3, 5, 2, 5, 2, 4};
+
+    // check the no of element present in an array
+
+    int n = sizeof(arr1) / sizeof(n);
+    int m = sizeof(arr2) / sizeof(m);
 
     // Function call
-    if (areEqual(arr1, arr2, N, M))
-        cout << "Yes";
+    if (areEqual(arr1, arr2, n, m))
+        cout << "These tow arrays are same!!";
     else
-        cout << "No";
+        cout << "These two arrays are different!!";
     return 0;
 }
