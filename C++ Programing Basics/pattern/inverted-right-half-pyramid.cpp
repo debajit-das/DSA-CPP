4181b831176b17a9afc32b9483b5dae23b40fafd
Debajit Das <debas1118@gmail.com>
2024-05-09 21:58:01 +0530

    Patterns

--- C++ Programing Basics/pattern/inverted-right-half-pyramid.cpp
+++ C++ Programing Basics/pattern/inverted-right-half-pyramid.cpp
@@ -7,11 +7,20 @@ int main()
     cout << "Enter the number of row: ";
     cin >> row;
 
-    for (int i = row; i >= 1; i--)
+    // for (int i = row; i >= 1; i--)
+    // {
+    //     for (int j = 1; j <= i; j++)
+    //     {
+    //         cout << "* ";
+    //     }
+    //     cout << endl;
+    // }
+
+    for (int i = 1; i <= row; i++)
     {
-        for (int j = 1; j <= i; j++)
+        for (int j = 1; j <= row - i; j++)
         {
-            cout << "*";
+            cout << "* ";
         }
         cout << endl;
     }
